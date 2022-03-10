bd1ca124a1fcefcf06b94f52ddcdb690de800012
Aitareko <77382573+Aitareko@users.noreply.github.com>
2022-03-10 21:11:55 +0100

    ✨ implement signing of files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 mod cli;
+mod sign;
 
 use clap::Parser;
 use crate::cli::Cli;
+use crate::sign::Sign;
 
 fn main() {
     let cli = Cli::parse();
+    if cli.verify {
+        perform_verify(cli)
+    } else {
+        perform_sign(cli);
+    }
+}
+
+fn perform_sign(cli: Cli) {
+    let sign_success = Sign::new(cli.file_path, cli.key_path, cli.signature_path).sign_file();
+    if sign_success {
+        println!("Signing file was successful!")
+    } else {
+        print!("Failed to sign file!")
+    }
+}
+
+fn perform_verify(cli: Cli) {
+    // still to be implemented
 }
---
+++ src/sign.rs
@@ -0,0 +1,67 @@
+use std::{fs, process::{Command, Output}};
+
+pub struct Sign {
+  file_path: String,
+  key_path: String,
+  signature_path: Option<String>,
+}
+
+impl Sign {
+
+  pub fn new(file_path: String, key_path: Option<String>, signature_path: Option<String>) -> Sign {
+    if let Some(provided_key_path) = key_path {
+      Sign { file_path, key_path: provided_key_path, signature_path }
+    } else {
+      let default_key_path = default_key_path();
+      Sign { file_path, key_path: default_key_path, signature_path }
+    }
+  }
+
+  pub fn sign_file(&self) -> bool {
+    let command_out = self.run_sign_command();
+    self.handle_sign_output(command_out)
+  }
+
+  fn run_sign_command(&self) -> Output {
+    Command::new("ssh-keygen")
+      .arg("-Y").arg("sign")
+      .arg("-f").arg(&self.key_path)
+      .arg("-n").arg(&self.file_path)
+      .output()
+      .expect("Failed to run ssh-keygen command")
+  }
+
+  fn handle_sign_output(&self, output: Output) -> bool {
+      if !output.stderr.is_empty() {
+        let stdout = String::from_utf8(output.stdout)
+          .expect("Failed to read stdout from ssh-keygen command");
+        self.handle_sign_stdout(stdout);
+        return true;
+      } else {
+        let stderr = String::from_utf8(output.stderr)
+          .expect("Failed to read stderr from ssh-keygen command");
+        self.handle_sign_stderr(stderr);
+        return false;
+      }
+  }
+
+  fn handle_sign_stdout(&self, stdout: String) {
+    if let Some(signature_file_path) = &self.signature_path {
+      fs::write(signature_file_path, stdout).expect("Failed to write signature to file");
+    } else {
+      let default_signature_file_path = format!("{}{}", self.file_path, ".sig");
+      fs::write(default_signature_file_path, stdout).expect("Failed to write signature to default file");
+    }
+  }
+
+  fn handle_sign_stderr(&self, stderr: String) {
+    println!("Failed signing file: {}", stderr);
+  }
+
+}
+
+fn default_key_path() -> String {
+  let home_dir = dirs::home_dir().expect("Failed to resolve home directory")
+    .to_str().expect("Failed to create string from home directory path").to_string();
+  format!("{}{}", home_dir, "/.ssh/file-sign.pub")
+}
